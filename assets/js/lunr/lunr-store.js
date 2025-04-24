a35b51e923cf6ed88fb0195aca77c3cb4f6c3cd4
ruyshy <ruyshy@users.noreply.github.com>
2025-04-24 07:59:12 +0000

    deploy: 50d14e070f8f47ad2f5835db1811a352f3f7c49c

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -116,6 +116,12 @@ var store = [{
         "title": "GitHub Blog Quiz Form - Liquid 플러그인으로 퀴즈 기능 구현하기",
         "excerpt":"Jekyll 블로그에서 Liquid 플러그인으로 퀴즈(객관식/주관식) 기능을 구현하고 인터랙티브한 문제 출제를 만드는 방법을 소개합니다. Quiz Form 미리보기 객관식 문제 1 : 객관식 미리보기 문제입니다. 문제의 테스트용 코드 블럭 입니다. 정답은 2번 1번 2번 3번 4번 제출 문제의 해설 보기 입니다. 주관식 test? 정답은 test 제출 test 문제 입니다. Quiz Form Code...","categories": ["blog"],
         "tags": ["GitHubBlog","quiz","plugin","liquid","jekyll","javascript"],
-        "url": "/blog/blog13-quiz-form/",
+        "url": "/blog/blog-13-quiz-form/",
+        "teaser": null
+      },{
+        "title": "GitHub Blog Card Link",
+        "excerpt":"Jekyll 블로그에서 외부 링크를 깔끔한 카드 형식으로 표시하고 싶다면 이 포스트를 참고하세요. Liquid 태그 기반 커스텀 플러그인을 이용해 GitHub Blog 카드 링크, 미리보기 링크 카드, OG 태그 기반 링크 프리뷰를 만드는 과정을 예제로 설명합니다. 링크 카드 미리 보기 Jekyll • Simple, blog-aware, static sites Transform your plain text into static...","categories": ["blog"],
+        "tags": ["GitHubBlog","link preview","liquid plugin","jekyll customization","javascript"],
+        "url": "/blog/blog14-card-link-pulgin/",
         "teaser": null
       }]
