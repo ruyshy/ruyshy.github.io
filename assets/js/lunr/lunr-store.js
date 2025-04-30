c4e9b371dca2c3fc1d0495681bc7bfe6ceb2f1d6
ruyshy <ruyshy@users.noreply.github.com>
2025-04-30 03:11:29 +0000

    deploy: c2ce853221494e1a931a07d9aed07269f6669c99

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -208,4 +208,10 @@ var store = [{
         "tags": ["assembly","disassembly","x64dbg","VisualStudio","어셈블리 분석","리버스엔지니어링"],
         "url": "/reversing_engineering/assembly/reversing1-disassembly1/",
         "teaser": null
+      },{
+        "title": "Disassembly 실전 분석편: if문과 for문 C++ → Assembly 흐름 이해",
+        "excerpt":"C++의 조건문과 반복문이 디스어셈블리에서 어떻게 구현되는지 궁금하신가요? 이 포스트에서는 Visual Studio 디버거를 활용해 if문, for문을 실제 실행파일 수준에서 분석하고, cmp, jmp, jge, add 등의 핵심 명령어 패턴을 정확히 짚어드립니다. 1. Visual Studio 설정 (이전이랑 동일) 빌드 모드: Release 플랫폼: x64 최적화: 끄기 (/Od) 그리고 C++ 코드 작성 후, 디버깅 시작해서...","categories": ["reversing_engineering"],
+        "tags": ["assembly","disassembly","VisualStudio","조건문 어셈블리","루프 구조","cmp","jmp","x64 디버깅"],
+        "url": "/reversing_engineering/reversing1-disassembly2/",
+        "teaser": null
       }]
