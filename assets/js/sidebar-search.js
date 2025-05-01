45ddf627b7d9c9542e00807ce4075e959dd63581
jaeheok <ruyshyttr@gmail.com>
2025-05-01 10:21:35 +0900

    Create sidebar-search.js

---
+++ assets/js/sidebar-search.js
@@ -0,0 +1,14 @@
+function SidebarSearchHandler() {
+  const query = document.getElementById("search-query").value;
+  const type = document.getElementById("search-type").value;
+
+  if (!query.trim()) return false;
+
+  let finalQuery = "";
+  if (type === "title") finalQuery = `title:${query}`;
+  else if (type === "tag") finalQuery = `tags:${query}`;
+  else finalQuery = query;
+
+  window.location.href = `/search/?q=${encodeURIComponent(finalQuery)}`;
+  return false;
+}
