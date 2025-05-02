2ae624abeac42454b1ab6dd6977e75ef4bb8c0f8
jaeheok <ruyshyttr@gmail.com>
2025-05-02 12:08:03 +0900

    create tag plugin

---
+++ _plugins/generate_tags.rb
@@ -0,0 +1,32 @@
+require 'set'
+
+module Jekyll
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      seen = Set.new
+      site.tags.each do |tag, posts|
+        next if seen.include?(tag)
+        seen.add(tag)
+
+        site.pages << TagPage.new(site, site.source, File.join('tags', tag), tag)
+      end
+    end
+  end
+
+  class TagPage < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag_archive.html')
+      self.data['tag'] = tag
+      self.data['title'] = "Tag: #{tag}"
+      self.data['entries_layout'] = 'list'
+    end
+  end
+end
